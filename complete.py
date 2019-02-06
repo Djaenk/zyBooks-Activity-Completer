af180ce1197877622374371690ac5c998a2ad5dd
Djaenk <Djaenk@users.noreply.github.com>
2019-02-06 17:37:16 -0600

    Fixed multiple major bugs stemming from a single type of error
    I was not aware starting an xpath with "//" would search the entire html file for what followed. Using ".//" allows for searching for all elements within a specified element.

--- complete.py
+++ complete.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait as wait
+from selenium.webdriver.common.keys import Keys
 import getpass
 import time
 
@@ -52,87 +54,98 @@ def sectionSelection(driver, chapter):
 		if(section.isnumeric()):
 			section_button = driver.find_element_by_xpath("//span[@class='section-title' and contains(text(), '" + chapter + "." + section + "')]")
 			section_button.click()
-			time.sleep(3)
-			playAnimations(driver)
-			completeMultipleChoice(driver)
-			completeShortAnswer(driver)
-			completeSelectionProblems(driver)
+			completeParticipationActivities(driver)
 			return_to_zybook = driver.find_element_by_xpath("//a[@href='/zybook/SMUCSE1342EvansSpring2019']")
 			return_to_zybook.click()
 			break
 		elif(section == "all"):
-			time.sleep(.5)
-			sections = driver.find_elements_by_xpath("/html/body/div[3]/div/div/section[1]/div[2]/ul/li[" + chapter + "]/div[2]/div/div[1]/ul/li[1]/div[1]/a/div/div[1]/span[@class='section-title' and contains(text(), '" + chapter + "')]")
-			for section in sections:
-				section.click()
-				playAnimations(driver)
-				completeMultipleChoice(driver)
-				completeShortAnswer(driver)
-				completeSelectionProblems(driver)
+			sections = driver.find_elements_by_xpath("//span[@class='section-title' and contains(text(), '" + chapter + ".')]")
+			for index, section in enumerate(sections, 1):
+				section = str(index)
+				section_link = driver.find_element_by_xpath("//span[@class='section-title' and contains(text(), '" + chapter + "." + section + "')]")
+				section_link.click()
+				completeParticipationActivities(driver)
 				return_to_zybook = driver.find_element_by_xpath("//a[@href='/zybook/SMUCSE1342EvansSpring2019']")
 				return_to_zybook.click()
 			break
 		else:
 			print("Please make a valid section selection.")
+
+def completeParticipationActivities(driver):
+	time.sleep(5)
+	playAnimations(driver)
+	time.sleep(1)
+	completeMultipleChoice(driver)
+	time.sleep(1)
+	completeShortAnswer(driver)
+	time.sleep(1)
+	completeSelectionProblems(driver)
 		
 def playAnimations(driver):
-	animation_players = driver.find_elements_by_xpath("//div[@class='interactive-activity-container animation-player-content-resource participation medium ember-view']")
+	animation_players = driver.find_elements_by_xpath("/html/body/div[3]/div/section/div/article/div/div[@class='interactive-activity-container animation-player-content-resource participation medium ember-view']")
+	animation_players += driver.find_elements_by_xpath("/html/body/div[3]/div/section/div/article/div/div[@class='interactive-activity-container animation-player-content-resource participation large ember-view']")
+	animation_players += driver.find_elements_by_xpath("/html/body/div[3]/div/section/div/article/div/div[@class='interactive-activity-container animation-player-content-resource participation small ember-view']")
 	for animation in animation_players:
-		double_speed = animation.find_element_by_xpath("//div[@class='speed-control ']")
+		double_speed = animation.find_element_by_xpath(".//div[@class='speed-control ']")
 		double_speed.click()
-		start_button = animation.find_element_by_xpath("//div[@class='start-button start-graphic']")
+		start_button = animation.find_element_by_xpath(".//div[@class='start-button start-graphic']")
 		start_button.click()
 		time.sleep(1)
 		while(True):
-			if(animation.find_elements_by_xpath("//div[@class='pause-button']")):
+			if(animation.find_elements_by_xpath(".//div[@class='pause-button']")):
 				time.sleep(.1)
 				continue
 			try:
-				play_button = animation.find_element_by_xpath("//div[@class='play-button  bounce']")
+				play_button = animation.find_element_by_xpath(".//div[@class='play-button  bounce']")
 				play_button.click()
 			except:
 				pass
-			if(animation.find_elements_by_xpath("//div[@class='play-button rotate-180 ']")):
+			if(animation.find_elements_by_xpath(".//div[@class='play-button rotate-180 ']")):
 				break
 		print("Completed animation activity")
 
 def completeMultipleChoice(driver):
 	multiple_choice_sets = driver.find_elements_by_xpath("//div[@class='interactive-activity-container multiple-choice-content-resource participation large ember-view']")
+	multiple_choice_sets += driver.find_elements_by_xpath("//div[@class='interactive-activity-container multiple-choice-content-resource participation medium ember-view']")
+	multiple_choice_sets += driver.find_elements_by_xpath("//div[@class='interactive-activity-container multiple-choice-content-resource participation small ember-view']")
 	for question_set in multiple_choice_sets:
-		questions = question_set.find_elements_by_xpath("//div[@class='question-set-question multiple-choice-question ember-view']")
+		questions = question_set.find_elements_by_xpath(".//div[@class='question-set-question multiple-choice-question ember-view']")
 		for question in questions:
-			choices = question.find_elements_by_xpath("//div[@class='zb-radio-button orange orange ember-view']")
+			choices = question.find_elements_by_xpath(".//label[@aria-hidden='true']")
 			for choice in choices:
 				choice.click()
-				choice.click()
-				if(question.find_elements_by_xpath("//div[@class='explanation has-explanation correct']")):
+				if(question.find_elements_by_xpath(".//div[@class='explanation has-explanation correct']")):
 					break
 		print("Completed multiple choice set")
 			
 def completeShortAnswer(driver):
 	short_answer_sets = driver.find_elements_by_xpath("//div[@class='interactive-activity-container short-answer-content-resource participation large ember-view']")
+	short_answer_sets += driver.find_elements_by_xpath("//div[@class='interactive-activity-container short-answer-content-resource participation medium ember-view']")
+	short_answer_sets += driver.find_elements_by_xpath("//div[@class='interactive-activity-container short-answer-content-resource participation small ember-view']")
 	for question_set in short_answer_sets:
-		questions = question_set.find_elements_by_xpath("//div[@class='question-set-question short-answer-question ember-view']")
+		questions = question_set.find_elements_by_xpath(".//div[@class='question-set-question short-answer-question ember-view']")
 		for question in questions:
-			show_answer_button = question.find_element_by_xpath("//div[@class='show-answer-button zb-button secondary ember-view']")
+			show_answer_button = question.find_element_by_xpath(".//button[@class='show-answer-button zb-button secondary ember-view']")
 			show_answer_button.click()
 			show_answer_button.click()
-			answer = question.find_element_by_xpath("//span[@class='forfeit-answer']").text
-			text_area = question.find_element_by_xpath("//textarea[@class='zb-text-area hide-scrollbar ember-text-area ember-view']")
+			answer = question.find_element_by_xpath(".//span[@class='forfeit-answer']").text
+			text_area = question.find_element_by_xpath(".//textarea[@class='zb-text-area hide-scrollbar ember-text-area ember-view']")
 			text_area.send_keys(answer)
-			check_button = question.find_element_by_xpath("//button[@class='check-button zb-button primary raised ember-view']")
+			check_button = question.find_element_by_xpath(".//button[@class='check-button zb-button primary raised ember-view']")
 			check_button.click()
 		print("Completed short answer set")
 
 def completeSelectionProblems(driver):
 	selection_problem_sets = driver.find_elements_by_xpath("//div[@class='interactive-activity-container detect-answer-content-resource participation large ember-view']")
+	selection_problem_sets += driver.find_elements_by_xpath("//div[@class='interactive-activity-container detect-answer-content-resource participation medium ember-view']")
+	selection_problem_sets += driver.find_elements_by_xpath("//div[@class='interactive-activity-container detect-answer-content-resource participation small ember-view']")
 	for question_set in selection_problem_sets:
-		questions = question_set.find_elements_by_xpath("//div[@class='question-set-question detect-answer-question ember-view']")
+		questions = question_set.find_elements_by_xpath(".//div[@class='question-set-question detect-answer-question ember-view']")
 		for question in questions:
-			choices = question.find_elements_by_xpath("//div[@class='explanation has-explanation correct']")
+			choices = question.find_elements_by_xpath(".//div[@class='explanation has-explanation correct']")
 			for choice in choices:
 				choice.click()
-				if(question.find_elements_by_xpath("//div[@class='explanation has-explanation correct']")):
+				if(question.find_elements_by_xpath(".//div[@class='explanation has-explanation correct']")):
 					break
 		print("Completed selection problem set")
 
