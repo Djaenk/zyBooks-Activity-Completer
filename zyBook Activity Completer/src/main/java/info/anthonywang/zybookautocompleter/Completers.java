50f665721edd90f754a2d712aeeb31bfdbe8fce5
awang1999 <awang1999@users.noreply.github.com>
2019-03-20 21:45:06 -0500

    More import removals

--- zyBook Activity Completer/src/main/java/info/anthonywang/zybookautocompleter/Completers.java
+++ zyBook Activity Completer/src/main/java/info/anthonywang/zybookautocompleter/Completers.java
@@ -8,8 +8,6 @@ import java.util.List;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import net.lightbody.bmp.BrowserMobProxy;
 import net.lightbody.bmp.core.har.Har;
